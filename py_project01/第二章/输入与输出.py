6c1e6d51899a9f2a5465d8b254456af55ab3c33a
黄智浩 <2367676838@qq.com>
2026-09-22 22:52:22 +0800

    day4：输入输出与运算符综合练习

---
+++ py_project01/第二章/输入与输出.py
@@ -0,0 +1,23 @@
+#获取键盘上输入的数据 -- input（）
+# name = input("请输入您的姓名：")
+# age = input("请输入您的年龄：")
+#
+# print(f"您的姓名是{name}，年龄是{age}")
+
+#案例:银行卡ATM取款
+#总金额
+# total=10000
+# #1.输入密码
+# password = input("请输入密码：")
+# print(f"密码正确,{password}")
+# #2.输入取款金额
+# num = input("请输入取款金额：")
+# #3.剩余金额 -->num转换int类型-->int()
+# print(f"剩余金额:{total-int(num)}")
+
+#练习：输入两个数字，计算两数之和，并输出
+a=input("请输入一个数字：")
+b=input("请再输入一个数字：")
+print(f"两数之和为:{int(a)+int(b)}")
+
+
